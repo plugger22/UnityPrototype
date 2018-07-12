09488d725e9266d0facfc35c7a507d0887a67474
cameron harris <cameron.tasmania@gmail.com>
2018-07-13 09:11:50 +1000

    Adding SO's for nodeCrisis and nodeDatapoints

---
+++ Assets/Scripts/SO Master/NodeCrisis.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// SO archetype for Node Crisis
+/// </summary>
+[CreateAssetMenu(menuName = "Node / NodeCrisis")]
+public class NodeCrisis : ScriptableObject
+{
+    [HideInInspector] public int nodeCrisisID;               //unique #, zero based, assigned automatically by DataManager.Initialise
+
+    public NodeDatapoint dataType;
+    [Tooltip("Used for shorter description")]
+    public string tag;
+    [Tooltip("Used for fuller description")]
+    [TextArea] public string description;
+    
+}
---
+++ Assets/Scripts/SO Master/NodeDatapoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// SO archetype for Node Datapoint. Name of SO is the type of Node datapoint, eg. "Security"
+/// </summary>
+[CreateAssetMenu(menuName = "Node / NodeDatapoint")]
+public class NodeDatapoint : ScriptableObject
+{
+    [HideInInspector] public int nodeDataID;               //unique #, zero based, assigned automatically by DataManager.Initialise
+}
