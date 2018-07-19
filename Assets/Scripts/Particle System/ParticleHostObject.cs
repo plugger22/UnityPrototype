8f510d0196738808b074ad9792c9cf872d0bc27d
cameron harris <cameron.tasmania@gmail.com>
2018-07-19 21:01:14 +1000

    learning how to script partcles in test scene.

---
+++ Assets/Scripts/Particle System/ParticleHostObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// test script to attach to host game object
+/// </summary>
+public class ParticleHostObject : MonoBehaviour
+{
+
+    public GameObject cube;
+
+    [HideInInspector] public ParticleLauncher launcher;
+
+
+    private void Awake()
+    {
+        launcher = cube.GetComponent<ParticleLauncher>();
+        if (launcher == null)
+        { Debug.LogWarning("Invalid launcher component (Null)"); }
+    }
+
+    private void Update()
+    {
+        if (launcher != null)
+        {
+            StartCoroutine("TestParticleSystem");
+        }
+    }
+
+
+    IEnumerator TestParticleSystem()
+    {
+        yield return new WaitForSecondsRealtime(5);
+        launcher.StartParticleSystem(20);
+        yield return new WaitForSecondsRealtime(5);
+        launcher.StopParticleSystem();
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/Particle System/ParticleLauncher.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Attach to a game object to control a particle system component
+/// </summary>
+public class ParticleLauncher : MonoBehaviour
+{
+    public ParticleSystem particleLauncher;
+
+    private void Update()
+    {
+        //deactivate Launcher at game start
+        if (particleLauncher != null)
+        { particleLauncher.gameObject.SetActive(false); }
+        else { Debug.LogWarning("Invalid particleLauncher (Null)"); }
+    }
+
+    /// <summary>
+    /// start emitting at a set rate of particles per frame
+    /// </summary>
+    /// <param name="particlesPerFrame"></param>
+    public void StartParticleSystem(int particlesPerFrame)
+    {
+        //keep input within reasonable parameters
+        particlesPerFrame = Mathf.Clamp(particlesPerFrame, 10, 40);
+        particleLauncher.gameObject.SetActive(true);
+        particleLauncher.Emit(particlesPerFrame);
+    }
+
+    /// <summary>
+    /// stop emitting
+    /// </summary>
+    public void StopParticleSystem()
+    {
+        particleLauncher.gameObject.SetActive(false);
+    }
+
+}
