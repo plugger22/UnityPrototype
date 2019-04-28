d3e7bf749f28453bdb0ee4f29193f557f821e7f2
Plugger22 <cameron.tasmania@gmail.com>
2019-04-28 21:39:57 +1000

    minor tweak

--- Assets/Scripts/Base Classes/Save.cs
+++ Assets/Scripts/Base Classes/Save.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 /// <summary>
 /// serializable class containing all game data for save / load file operations
+/// NOTE: does NOT derive from Monobehaviour
 /// </summary>
 [System.Serializable]
-public class Save : MonoBehaviour
+public class Save
 {
 
 
