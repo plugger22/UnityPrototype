3efb6e78b3a6c608bf18d5fa550c8b08fa580548
Plugger22 <cameron.tasmania@gmail.com>
2019-10-27 20:29:46 +1100

    testing a scrolling background (test scene)

---
+++ Assets/Scripts/Utilities/ScrollingBackground.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// test method to scroll background vertically
+/// </summary>
+public class ScrollingBackground : MonoBehaviour
+{
+
+    public float scrollSpeed;
+    public Renderer renderer;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        renderer.material.mainTextureOffset += new Vector2(0f, scrollSpeed * Time.deltaTime);
+	}
+}
