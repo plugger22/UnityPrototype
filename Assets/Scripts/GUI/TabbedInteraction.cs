671e2309cbd686d6c02570413915d0b878b6a882
cameron harris <cameron.tasmania@gmail.com>
2020-11-21 12:30:54 +1100

    changed to tabbedInteraction.cs for tabbed UI

---
+++ Assets/Scripts/GUI/TabbedInteraction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// handles Actor tab interaction for TabbedUI
+/// </summary>
+public class TabbedInteraction : MonoBehaviour
+{
+
+    public Image background;
+    public Image portrait;
+    public TextMeshProUGUI title;
+
+
+
+    public void Awake()
+    {
+        Debug.AssertFormat(background != null, "Invalid background (Null) for {0}", this);
+        Debug.AssertFormat(portrait != null, "Invalid portrait (Null) for {0}", this);
+        Debug.AssertFormat(title != null, "Invalid title (Null) for {0}", this);
+    }
+
+}
