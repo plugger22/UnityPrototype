0fb28a95b849c9ac2f5e7f4a24e704d7d1ef0370
cameron harris <cameron.tasmania@gmail.com>
2021-01-22 17:03:57 +1100

    minor tweaks

--- Assets/Scripts/SO Master/ActorDraft.cs
+++ Assets/Scripts/SO Master/ActorDraft.cs
@@ -8,17 +8,25 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Actor / ActorDraft")]
 public class ActorDraft : ScriptableObject
 {
+    [Header("Names")]
     public string actorName;
     public string firstName;
     public string lastName;
-    public Sprite sprite;
 
+    [Header("Details")]
+    public Sprite sprite;
     public ActorArc arc;
     public Trait trait;
     public int level;
     public int power;
-    public string backstory;
+
+    [Header("Status")]
     public ActorDraftStatus status;
     public ActorDraftSex sex;
 
+    [Header("Backstory")]
+    [Tooltip("Four lines of text max (backstory1 is added on with a blank line between")]
+    [TextArea(3,5)] public string backstory0;
+    [Tooltip("Two lines of text max (this is added on to backstory0 with a blank line between")]
+    [TextArea(1, 2)] public string backstory1;
 }
