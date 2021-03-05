5eda76f18adc49857f9d38ab1805514781d10a4e
cameron harris <cameron.tasmania@gmail.com>
2021-03-05 12:35:10 +1100

    working on framework for help.SO and infoDump system. In hand.

---
+++ Assets/Scripts/SO Master/Help.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Main help SO data for the help system (this is independent of the tutorial help system)
+/// </summary>
+[CreateAssetMenu(menuName = "Help / Help")]
+public class Help : ScriptableObject
+{
+
+    public string header;
+
+    [Tooltip("Layout used by helpUI.cs")]
+    public Layout layout;
+
+    [Tooltip("Info sprites. Add appropriately according to the layout, eg. if the layout requires two then use the first two sprite slots and they will be used in that order")]
+    public Sprite sprite0;
+
+}
---
+++ Assets/Scripts/SO Master/Layout.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// enum SO that defines a particular layout for a help UI
+/// </summary>
+[CreateAssetMenu(menuName = "Help / Layout")]
+public class Layout : ScriptableObject
+{
+    [Tooltip("Developer notes only, not used inGame")]
+    [TextArea] public string notes;
+
+}
